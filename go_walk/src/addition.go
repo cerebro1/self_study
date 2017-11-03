baaa8024f532042b12eae306745921dda95cc99b
Sonali Srivastava <sonalis@one.com>
2017-11-03 09:51:14 +0530

    Named return values

--- go_walk/src/addition.go
+++ go_walk/src/addition.go
@@ -3,9 +3,10 @@ package main
 import "fmt"
 
 
-func add( num1, num2 int) int {
-	num3 := num1 + num2
-	return num3
+func add( num1, num2 int) (num4, num3 int) {
+	num3 = num1 + num2
+	num4 = num1 + num3
+	return
 }
 
 func main(){
